8ec4576ec8861b8de234523ed261dcbcbc4dd34a
Bijay Sharma <bijaysharma8335@gmail.com>
2023-03-18 16:42:23 +0530

    Update Contact Page Form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import FormInput from "../components/FormInput";
 import "./Contact.css";
 const Contact = () => {
+    const [credentials, setCredentials] = useState({
+        displayName: "",
+        email: "",
+        phone: "",
+        subject: "",
+        message: "",
+    });
+    const { displayName, email, phone, subject, message } = credentials;
+
+    const handleChange = () => {};
     return (
         <div className="contact_container">
             <div className="page-heading">
@@ -14,52 +25,61 @@ const Contact = () => {
                     </div>
                     <div className="row">
                         <div className="col-md-6 mb-50">
-                            <span className="input">
-                                <input
-                                    type="text"
-                                    name="cf-name"
-                                    required
-                                    className="input_field"
-                                    id="cf-name"
-                                />
-                                <label className="input_label" for="cf-name">
-                                    Name
-                                </label>
-                            </span>
+                            <FormInput
+                                name="name"
+                                type="text"
+                                value={displayName}
+                                handleChange={handleChange}
+                                label="Name"
+                                required
+                            />
                         </div>
                         <div className="col-md-6 mb-50">
-                            <span className="input">
-                                <input
-                                    type="email"
-                                    name="email"
-                                    required
-                                    className="input_field"
-                                    id="cf-email"
-                                />
-                                <label className="input_label" for="cf-email">
-                                    Email
-                                </label>
-                            </span>
+                            <FormInput
+                                name="email"
+                                type="email"
+                                value={email}
+                                handleChange={handleChange}
+                                label="Email"
+                                required
+                            />
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col-md-6 mb-50">
-                            <span className="input">
-                                <input type="text" name="phone" required className="input_field" />
-                                <label className="input_label">Phone</label>
-                            </span>
+                            <FormInput
+                                name="phone"
+                                type="number"
+                                value={phone}
+                                handleChange={handleChange}
+                                label="Phone"
+                                required
+                            />
                         </div>
                         <div className="col-md-6 mb-50">
-                            <span className="input">
-                                <input type="text" name="email" required className="input_field" />{" "}
-                                <label className="input_label">Subject</label>
-                            </span>
+                            <FormInput
+                                name="subject"
+                                type="text"
+                                value={subject}
+                                handleChange={handleChange}
+                                label="Subject"
+                                required
+                            />
                         </div>
                     </div>
 
                     <div className="col-md-12 text-center mb-5">
-                        <span className="input">
+                        <FormInput
+                            name="message"
+                            type="text"
+                            value={message}
+                            handleChange={handleChange}
+                            label="How can we help you?"
+                            required
+                           
+                        />
+                        {/* <span className="input">
                             <textarea
                                 rows="5"
                                 name="Message"
@@ -70,16 +90,22 @@ const Contact = () => {
                             <label className="input_label" for="help-msg">
                                 How can we help you?
                             </label>
-                        </span>
-
+                        </span> */}
                         <button type="submit" className="submit-btn ">
                             Send Message
                         </button>
                     </div>
-                    <div className="row">
-                        <div className="col-md-4"></div>
-                        <div className="col-md-4"></div>
-                        <div className="col-md-4"></div>
+                    <div className="row location-iframe">
+                        <iframe
+                            title="myLocation"
+                            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d971.6583759390658!2d77.58143502916803!3d13.05898479942493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDAzJzMyLjQiTiA3N8KwMzQnNTUuMSJF!5e0!3m2!1sen!2sin!4v1679130414425!5m2!1sen!2sin"
+                            width={500}
+                            height={300}
+                            style={{ border: "0" }}
+                            allowfullscreen=""
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"
+                        />
                     </div>
                 </div>
             </form>
