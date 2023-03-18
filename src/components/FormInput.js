8fc0b5ec3dcda891b6f2a49334b126dbefef211c
Bijay Sharma <bijaysharma8335@gmail.com>
2023-03-18 17:07:17 +0530

    Update Header Navigation Styling

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -2,23 +2,22 @@ import React from "react";
 import "./FormInput.css";
 
 const FormInput = ({ handleChange, label, ...otherProps }) => {
+    console.log(otherProps.name);
     return (
-      <div className="group">
+        <div className="group">
+            {otherProps.name === "message" ? (
+                <textarea className="form-input " onChange={handleChange} {...otherProps} />
+            ) : (
+                <input className="form-input" onChange={handleChange} {...otherProps} />
+            )}
 
-        
-        <input className="form-input" onChange={handleChange} {...otherProps} />
-  
-        {label ? (
-          <label
-            className={`${
-              otherProps.length ? "shrink" : ""
-            } form-input-label`}
-          >
-            {label}
-          </label>
-        ) : null}
-      </div>
+            {label ? (
+                <label className={`${otherProps.length ? "shrink" : ""} form-input-label`}>
+                    {label}
+                </label>
+            ) : null}
+        </div>
     );
-  };
+};
 
 export default FormInput;
